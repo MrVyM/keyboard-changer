6574c028460da7e5e147e4915cdfc32b6e39a37c
Vianney Marticou <vianney.marticou@epita.fr>
2022-10-27 23:06:57 +0200

    [FEATURES] Save and restore the oldest terminal

--- keyboard-changer.sh
+++ keyboard-changer.sh
@@ -1,9 +1,11 @@
-printf '\e[?1049h'
+printf '\e[?1049h' # save terminal
 
-printf '\e[2J'
+printf '\e[2J' # clear the terminal
 
 while true
 do
     read -n1 
     echo $REPLY 
 done
+
+printf '\e[?1049l' # restore terminal 
